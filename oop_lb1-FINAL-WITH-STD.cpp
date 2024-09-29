fe759e56754b18e9fa92cea3e9af8c3d48fdda8c
hell-gus <144446200+hell-gus@users.noreply.github.com>
2024-09-29 15:30:10 +0300

    Add files via upload

---
+++ oop_lb1-FINAL-WITH-STD.cpp
@@ -0,0 +1,275 @@
+
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+enum class Orientation{ Horizontal, Vertical }; //ориентация корабля на поле
+enum class SegmentState { Whole, Injured, Destroyed }; //состояние сегмента корабля
+enum class CellFieldStatus{ Unknown, Empty, Ship}; //статус клетки поля
+enum class FieldType { My, Enemy };
+ 
+class Ship{
+private:    
+    int length;//сколько палуб
+    Orientation orientation;
+    std::vector<SegmentState> segments;
+    int index_of_ship;
+    
+public:
+    Ship(int length, Orientation orientation)
+    : length(length), orientation(orientation), segments(length, SegmentState::Whole)
+    {
+        if (!( 1 <= length && length <= 4 )){
+            throw std::invalid_argument("Invalid type of ship, length might be only in the range 1 to 4");
+        }
+    }
+
+    int getLength() const { return length; }
+    void setIndex(int index) {this->index_of_ship = index;}
+    int getIndex () const { return index_of_ship; }
+
+    Orientation getOrientation() const { return orientation; }
+
+    SegmentState getSegmentState(int index_of_ship) const {
+        if ( 0 > index_of_ship || index_of_ship >= length ){
+            throw std::invalid_argument("Invalid segment index, it should be like amount of decks");
+        }
+        return segments[index_of_ship];
+    }
+
+    SegmentState changeSegmentState(int index_of_segment) {
+        if ( segments[index_of_segment] == SegmentState::Whole  ){
+            segments[index_of_segment] = SegmentState::Injured;
+        }else if ( segments[index_of_segment] == SegmentState::Injured  ){
+            segments[index_of_segment] = SegmentState::Destroyed;
+        }
+
+        return segments[index_of_segment];
+    }
+
+};
+
+class Ship_Manager{
+private:
+    std::vector<Ship> ships;
+
+public:
+    Ship_Manager( std::vector<int> length_of_ship, int count_of_ships )
+    {
+        if ( count_of_ships != length_of_ship.size() ) {
+            throw std::invalid_argument("Number of ships and size of the length vector must be equal");
+        }
+
+        for ( int i = 0; i < count_of_ships; i++) {
+            ships.resize(count_of_ships, Ship{length_of_ship[i], Orientation::Horizontal});
+            ships[i].setIndex(i);
+        }
+    }
+    
+    Ship& getShip( int index ){return ships[index];}
+    std::vector<Ship>& getArrayShips(){return ships;}
+
+    void printStateSegment( const Ship& ship) const //  ХУЙНЯ
+    {
+        for ( int i = 0; i < ship.getLength(); i++){
+            SegmentState state = ship.getSegmentState(i);
+            std::cout << "Segment " << i << ": " << static_cast<int>(state) << std::endl;
+        }
+    }
+
+
+        
+
+};
+
+class Field{
+private:
+    int width;
+    int height;
+    std::vector<std::vector<CellFieldStatus>> field;
+    std::vector<std::vector<int>> cells;
+    FieldType type_of_field;
+    bool My;
+
+ public:
+    Field( int width, int height, bool My )
+    {
+        if ( width <= 0 || height <= 0){
+            throw std::invalid_argument("width or height cannot be be less than 0");
+        }
+        this->width = width;
+        this->height = height;
+        this->My = My;
+        this->field = std::vector<std::vector<CellFieldStatus>>(height, std::vector<CellFieldStatus>(width, CellFieldStatus::Unknown));
+        this->cells = std::vector<std::vector<int>>(height, std::vector<int>(width, -1));
+    }
+
+    bool PutShip( const Ship& ship, Orientation orientation, int x, int y )
+    {
+        int length = ship.getLength();
+
+        if (!isPlaceValid(ship, x, y, orientation)){
+            return false;
+        }
+
+        if ( orientation == Orientation::Horizontal){
+            for( int i = 0; i < ship.getLength(); i++){
+                cells[y][x + i] = ship.getIndex();
+                field[y][x + i] = CellFieldStatus::Ship;
+            }
+        } else {
+            for( int i = 0; i < ship.getLength(); i++){
+                cells[y + i][x] = ship.getIndex();
+                field[y + i][x] = CellFieldStatus::Ship;
+            }
+        }
+        return true;
+        
+    }
+
+    bool isInField(const Ship& ship,int x, int y, Orientation orientation) const {
+        if ( orientation == Orientation::Horizontal){
+            return x >= 0 && x + ship.getLength() <= width && y>= 0 && y <height;
+        } else {
+            return y>= 0 && y + ship.getLength() <= height && x >= 0 && x < width;
+        }
+    }
+
+    bool isCellEmpty(int x, int y) const{
+        return field[y][x] != CellFieldStatus::Ship;
+    }
+
+    bool isPlaceValid(const Ship& ship, int x, int y, Orientation orientation){
+        //выход за границы поля
+        if ( !isInField(ship, x, y, orientation)){
+            return false;
+        }
+        
+        //пересечение кораблей
+        //цикл перебора по сегментам
+
+        for ( int i = 0; i < ship.getLength(); ++i){
+            int checkX = (orientation == Orientation::Horizontal) ? x + i : x;
+            int checkY = (orientation == Orientation::Horizontal) ? y : y + i;
+
+            if (!isCellEmpty(checkX, checkY)){
+                return false;
+            }
+        }
+
+        for ( int dx = -1; dx <= 1; ++dx){
+            for ( int dy = -1; dy <= 1; ++dy){
+                for ( int i = 0; i < ship.getLength(); ++i){
+                    int checkX = (orientation == Orientation::Horizontal) ? x + i : x;
+                    int checkY = (orientation == Orientation::Horizontal) ? y : y + i;
+
+                    if (isInField(ship, checkX + dx, checkY + dy, orientation) && !isCellEmpty(checkX + dx, checkY + dy)){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    int getIndexShip(int index, int x, int y){
+        int index_segment = 0;
+        while ( x > 0 && cells[y][x - 1] == index){
+            x--;
+            index_segment++;
+        }
+        while ( y > 0 &&  cells[y - 1][x] == index){
+            y--;
+            index_segment++;
+        }
+        return index_segment;
+
+    }
+
+    void attackCell(int y, int x, Ship_Manager& manager){
+        int index = cells[y][x];
+        if( index != -1){
+            manager.getShip(index).changeSegmentState(getIndexShip(index, x, y));
+            field[y][x] = CellFieldStatus::Ship;
+            return;
+         }
+        
+        field[y][x] = CellFieldStatus::Empty;
+    }
+
+    void drawField(Ship_Manager& manager) {//норм только для 10*10
+            char rowLabels[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O','P', 'R', 'S', 'T'};
+            std::cout << "  ";  // Пробел для выравнивания с буквами строк
+            for (int i = 1; i < width + 1; ++i) {
+                std::cout << i << " "; 
+            }
+            std::cout << '\n';
+            for (int y = 0; y < height; ++y) {
+                std::cout << rowLabels[y] << " ";  
+                for (int x = 0; x < width; ++x) {
+                    int index = cells[y][x];
+                    if ( index != -1){
+                        if ( manager.getShip(index).getSegmentState(getIndexShip(index, x, y)) == SegmentState::Whole){
+                            if (My)
+                                std::cout << "# ";
+                            else
+                                std::cout << "~ ";
+                        }else if (manager.getShip(index).getSegmentState(getIndexShip(index, x, y)) == SegmentState::Injured){
+                            std::cout << "+ ";
+                        }else if (manager.getShip(index).getSegmentState(getIndexShip(index, x, y)) == SegmentState::Destroyed) {
+                            std::cout << "X ";
+                        }
+                    }else if (field[y][x] == CellFieldStatus::Empty){
+                            std::cout << "0 ";//в меня не стреляли
+                    } else {
+                            std:: cout << "~ ";//в врага стреляли
+                    }
+                }
+                std::cout << "\n";
+            }
+        }
+};
+int main(){
+    Field play1(10, 10, true);
+    std::vector<int> sizes_my = {2, 3};
+    Ship_Manager ships_my(sizes_my, 2);
+    std::vector<std::pair<int, int>> start_coords_my = {{2, 2}, {5, 5}};
+
+    int index_my = 0;
+
+    for(auto& ship_my: ships_my.getArrayShips()){
+        play1.PutShip(ship_my, Orientation::Vertical, start_coords_my[index_my].first, start_coords_my[index_my].second);
+        index_my++;
+    }
+
+    Field play2 (10, 10, false);
+    std::vector<int> sizes_enemy = {2, 3};
+    Ship_Manager ships_enemy(sizes_enemy, 2);
+    std::vector<std::pair<int, int>> start_coords_enemy = {{2, 2}, {5, 5}};
+
+    int index_enemy = 0;
+
+    for(auto& ship_enemy: ships_enemy.getArrayShips()){
+        play2.PutShip(ship_enemy, Orientation::Vertical, start_coords_enemy[index_enemy].first, start_coords_enemy[index_enemy].second);
+        index_enemy++;
+    }
+
+
+    play1.attackCell(9, 2, ships_my);
+
+    play1.attackCell(2, 2, ships_my);
+    play1.attackCell(2, 2, ships_my);
+
+    play1.attackCell(3, 2, ships_my);
+
+
+    play2.attackCell(2, 2, ships_enemy);
+
+    play1.drawField(ships_my);
+    play2.drawField(ships_enemy);
+    return 0;
+
+    //НО МЫ АТАКУЕМ И СТАВИМ КОРАБЛИ НЕ ПО КООРДИНАТАМ А ПО ИНДЕКСАМ
+    //ДАЛЕЕ ЭТО БУДЕТ ИСПРАВЛЕННО ПРИ НАПИСАНИИ ВВОДА ОТ ПОЛЬЗОВАТЕЛЯ
+}
+
